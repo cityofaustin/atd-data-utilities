d116d53c719665b135c51eb314295df91265110c
Sergio <sergiogcx@gmail.com>
2019-06-20 17:21:42 -0500

    Added atd-knack-util

---
+++ atd-knack-util/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atd-knack-util",
+    version="0.0.2",
+    author="City of Austin",
+    author_email="transportation.data@austintexas.gov",
+    description="Python utilities for interacting with Knack applications.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cityofaustin/atd-data-utilities/tree/master/atd-knack-util",
+    install_requires=[
+        "arrow"
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta", 
+    ),
+)
+
