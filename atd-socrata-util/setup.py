2a3c4aee1bb5597d7e4a63eb1b4b6dde1a36c0af
Sergio <sergiogcx@gmail.com>
2019-06-20 17:25:03 -0500

    Added atd-socrata-util

---
+++ atd-socrata-util/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atd-socrata-util",
+    version="0.0.3",
+    author="City of Austin",
+    author_email="transportation.data@austintexas.gov",
+    description="Utilities interacting with the Socrata Open Data API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cityofaustin/atd-data-utilities/tree/master/atd-socrata-util",
+    install_requires = [
+        "requests",
+        "atd-data-util"
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta", 
+    ),
+)
+
