36ed18d7fba33d16ffb51aae4f2f8a0502e5104f
Sergio <sergiogcx@gmail.com>
2019-06-20 14:27:30 -0500

    Added a second package for testing

---
+++ atd-email-util/README.md
@@ -0,0 +1,23 @@
+# atd-email-util (emailutil)
+Python utilities for sending emails with a gmail account.
+
+
+## Installation
+
+Master branch:
+
+```
+pip install atd-email-util
+```
+
+Development branch:
+
+```
+pip install atd-email-util-dev
+```
+
+## License
+
+As a work of the City of Austin, this project is in the public domain within the United States.
+
+Additionally, we waive copyright and related rights of the work worldwide through the [CC0 1.0 Universal public domain dedication](https://creativecommons.org/publicdomain/zero/1.0/).
---
+++ atd-email-util/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atd-email-util",
+    version="0.0.1",
+    author="City of Austin",
+    author_email="transportation.data@austintexas.gov",
+    description="Python utilities for sending emails with a gmail account.",
+    install_requires=[
+      'yagmail',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cityofaustin/atd-data-utilities/tree/master/emailutil",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta", 
+    ),
+)
