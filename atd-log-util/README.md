0c4ceb0ab842e7cd554b126e4c2b7439d13e14fb
Sergio <sergiogcx@gmail.com>
2019-06-20 17:19:03 -0500

    Added atd-log-util

---
+++ atd-log-util/README.md
@@ -0,0 +1,24 @@
+# atd-log-util (logutil)
+
+Python utilities for common logging tasks.
+
+## Installation
+
+
+Master branch:
+
+```
+pip install atd-log-util
+```
+
+Development branch:
+
+```
+pip install atd-log-util-dev
+```
+
+
+## License
+As a work of the City of Austin, this project is in the public domain within the United States.
+
+Additionally, we waive copyright and related rights of the work worldwide through the [CC0 1.0 Universal public domain dedication](https://creativecommons.org/publicdomain/zero/1.0/).
---
+++ atd-log-util/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atd-log-util",
+    version="0.0.2",
+    author="City of Austin",
+    author_email="transportation.data@austintexas.gov",
+    description="Python utilities for common logging tasks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cityofaustin/atd-data-utilities/tree/master/atd-log-util",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta", 
+    ),
+)
+
