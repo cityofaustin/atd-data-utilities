a57835a38253f5cc770d7bbae52d1de89e10f3d3
Sergio <sergiogcx@gmail.com>
2019-06-21 13:36:05 -0500

    Added find_packages to atd-args-util

--- atd-args-util/setup.py
+++ atd-args-util/setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="atd-args-util",
-    version="0.0.3",
+    version="0.0.4",
     author="City of Austin",
     author_email="transportation.data@austintexas.gov",
     description="Reusable argument parser for Python scripts.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cityofaustin/atd-data-utilities/tree/master/atd-args-util",
+    packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: Public Domain",
