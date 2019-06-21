8d60530f82df0abb77ca8624b8ef8e6d7adedae0
Sergio <sergiogcx@gmail.com>
2019-06-21 13:37:38 -0500

    Added find_packages to atd-log-util

--- atd-log-util/setup.py
+++ atd-log-util/setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="atd-log-util",
-    version="0.0.2",
+    version="0.0.3",
     author="City of Austin",
     author_email="transportation.data@austintexas.gov",
     description="Python utilities for common logging tasks.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cityofaustin/atd-data-utilities/tree/master/atd-log-util",
+    packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: Public Domain",
