549b64d3a4ada10b217a8f521e50c8b7915c65fd
John Clary <john.clary@austintexas.gov>
2019-10-18 15:57:08 -0500

    bump version

--- atd-socrata-util/setup.py
+++ atd-socrata-util/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="atd-socrata-util",
-    version="0.0.7",
+    version="0.0.8",
     author="City of Austin",
     author_email="transportation.data@austintexas.gov",
     description="Utilities interacting with the Socrata Open Data API.",
